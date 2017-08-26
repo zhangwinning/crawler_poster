f8f485e667a4bfeff07f1271687f1e4e71682c91
张文宁 <zhangwenning@lingxi.co>
2017-08-26 20:46:22 +0800

    :tada: update file

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const async = require('async');
 const arrange = require('./arrange');
 const log4js = require('log4js').getLogger("begin crawler");
 
-let postersDir = path.join(__dirname, 'source');
 
 let loveURL = "http://dianying.fm/search/?genre=%E7%88%B1%E6%83%85&p=";
 
@@ -18,27 +17,38 @@ let download = function (uri, filename, cb) {
     });
 };
 
+let getSrc = function (body) {
+    let $ = cheerio.load(body);
+    let list = [];
+    $('ul[class="fm-result-list"]').find('li > div > a > img').each(function (index, element) {
+        list.push($(element).attr('src'));
+    })
+    return list;
+}
+
+// 获取`爬虫爬取的图片在本地的保存位置`。
+let getPosterPath = function (src) {
+    let postersDir = path.join(__dirname, 'source');
+    let fileName = src.split('/')[src.split('/').length - 1].split('-')[0];
+    let posterPath = path.join(postersDir, fileName);
+    return posterPath;
+}
 
 let begin = () => {
     let downLoads = 0;
     if (!process.argv[2] && !process.argv[3] && !process.argv[4]) {
         log4js.warn(`请从命令行中输入页数,宽度, 高度 forExample: node dingying.js 3 4 5 `);
-        process.exit();
+        process.exit(0);
     }
     let pages = process.argv[2];
     let loop = new Array(parseInt(pages));      //设置要循环的次数;
 
     async.eachOfLimit(loop, 2, (page, index, cbPage) => {
-        request(loveURL + (index + 7), function (error, response, body) {
-            let $ = cheerio.load(body);
-            let list = [];
-            $('ul[class="fm-result-list"]').find('li > div > a > img').each(function (index, element) {
-                list.push($(element).attr('src'));
-            })
+        request(loveURL + (index + 1), function (error, response, body) {
+            let list = getSrc(body);
             async.eachSeries(list, (src, cbSrc) => {
                 log4js.info(src, "start ...");
-                let fileName = src.split('/')[src.split('/').length - 1].split('-')[0];
-                let posterPath = path.join(postersDir, fileName);
+                let posterPath = getPosterPath(src)
                 download(src, posterPath, function (err) {
                     if (err) log4js.error(err);
                     downLoads++;
