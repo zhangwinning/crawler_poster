cb3782a807b4d73b70782ace1cf87ca8fb5e807e
张文宁 <zhangwenning@lingxi.co>
2017-08-26 18:40:46 +0800

    :tada: update file

--- gitCommit.sh
+++ gitCommit.sh
@@ -5,6 +5,6 @@ then
     commit_message=":tada: update file" 
 fi    
 echo "$commit_message"
-git add . 
+git add index.js arrange.js package.json README.md
 git commit -m "$commit_message"
 git push
